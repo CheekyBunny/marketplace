cad193810458be76b0f1530b07c44ea9e50b0cb4
StacieRFox <stasyria@mail.ru>
2018-06-22 23:10:44 +0300

    Switch to SASS and WebPack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        })
+    ]
+};
